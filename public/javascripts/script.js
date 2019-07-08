a4788bd292365287f5abce8b1cc0ded8974088e3
Tina V <fvasilovschi@gmail.com>
2019-07-08 20:30:09 +0300

    fetch events from db

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,27 @@
-fetch('events').then(function (r){
+fetch('events').then(function (r) {
     return r.json();
-}).then(function(data){
-    console.log('volunteers: ', data);
-})
+}).then(function (events) {
+    console.log('events: ', events);
+    //TODO - display events
+    display(events);
+});
+
+//TODO - create display function
+function display(events) {
+    var list = events.map(function(event) {
+        return `<tr data-id="${event.id}">
+        <td>${event.name}</td>
+        <td>${event.date}</td>
+        <td>${event.needed}</td>
+        <td>${event.applied}</td>
+        <td>
+                <button type="submit" a href="#" class="apply">Apply</a></button>
+            </td>
+    </tr>`;
+    });
+    document.querySelector("#event tbody").innerHTML = list.join("");
+}
+//TODO - parse every event and create row
+//TODO - save every row in list of rows
+//TODO - query and get empty table
+//TODO - add list of rows to empty table
